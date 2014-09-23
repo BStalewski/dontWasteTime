290e499db2559073c9177764a138adeb9d9132e6
Bartosz Stalewski <bartekstalewski@gmail.com>
2014-09-23 08:17:11 +0200

    Change lang to pl, add natural time

--- webapp/crawler_app/crawler_app/settings.py
+++ webapp/crawler_app/crawler_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -68,7 +69,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl'
 
 TIME_ZONE = 'Europe/Warsaw'
 
