4d038b65cde105f09f950ec9077b4c4b2f3f1098
Bartosz Stalewski <bartekstalewski@gmail.com>
2014-09-25 08:23:00 +0200

    Change db host to localhost

--- webapp/crawler_app/crawler_app/settings.py
+++ webapp/crawler_app/crawler_app/settings.py
@@ -67,7 +67,7 @@ DATABASES = {
         'NAME': 'crawlerdb',
         'USER': 'crawleruser',
         'PASSWORD': 'crawl_lova',
-        'HOST': '192.168.51.122',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
