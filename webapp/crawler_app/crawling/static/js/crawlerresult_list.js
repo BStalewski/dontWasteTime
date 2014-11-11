f0fd2bb5cf515a4957cbe50f1b9135b2c18366d3
Bartosz Stalewski <bartekstalewski@gmail.com>
2014-11-12 00:25:51 +0100

    Fix bug when clicking on icon

--- webapp/crawler_app/crawling/static/js/crawlerresult_list.js
+++ webapp/crawler_app/crawling/static/js/crawlerresult_list.js
@@ -2,15 +2,26 @@ var crawler = (function() {
     var that = {};
 
     that.accept = function(event) {
-        update_status(event.target, '/crawling/accept/');
+        update_status(event, '/crawling/accept/');
     };
 
     that.ignore = function(event) {
-        update_status(event.target, '/crawling/ignore/');
+        update_status(event, '/crawling/ignore/');
     };
 
-    function update_status(button, url_prefix) {
-        var id = parseInt(button.id.split('-')[2]);
+    function update_status(event, url_prefix) {
+        function find_button(event) {
+            var element;
+            for (var i = 0; i < event.path.length; i += 1) {
+                element = $(event.path[i]);
+                if (element.prop('tagName') === 'BUTTON') {
+                    return element;
+                }
+            }
+        }
+
+        var button = find_button(event);
+        var id = parseInt(button.prop('id').split('-')[2]);
         var url = url_prefix + id + '/';
         $.post(url, {}, function() {
             var row = $('#result-' + id);
