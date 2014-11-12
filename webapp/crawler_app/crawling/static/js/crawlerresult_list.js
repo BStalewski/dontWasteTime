d4937098ffc3b7e6f797be71636fc5269391bbfb
Bartosz Stalewski <bartekstalewski@gmail.com>
2014-11-12 23:22:11 +0100

    Fix table presentation and event handling in Firefox

--- webapp/crawler_app/crawling/static/js/crawlerresult_list.js
+++ webapp/crawler_app/crawling/static/js/crawlerresult_list.js
@@ -2,25 +2,23 @@ var crawler = (function() {
     var that = {};
 
     that.accept = function(event) {
-        update_status(event, '/crawling/accept/');
+        update_status(event.target, '/crawling/accept/');
     };
 
     that.ignore = function(event) {
-        update_status(event, '/crawling/ignore/');
+        update_status(event.target, '/crawling/ignore/');
     };
 
-    function update_status(event, url_prefix) {
-        function find_button(event) {
-            var element;
-            for (var i = 0; i < event.path.length; i += 1) {
-                element = $(event.path[i]);
-                if (element.prop('tagName') === 'BUTTON') {
-                    return element;
-                }
+    function update_status(target, url_prefix) {
+        function find_button() {
+            var element = $(target);
+            while (element.prop('tagName') !== 'BUTTON') {
+                element = element.parent();
             }
+            return element;
         }
 
-        var button = find_button(event);
+        var button = find_button();
         var id = parseInt(button.prop('id').split('-')[2]);
         var url = url_prefix + id + '/';
         $.post(url, {}, function() {
